6bda5700eaa156e3d22e3b61a85a7536b3879ae8
majassi <46198752+majassi@users.noreply.github.com>
2020-11-07 20:36:33 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,107 @@
+//Quiz
+const correctAnswers = ['B', 'B', 'B', 'B'];
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  let score = 0;
+  const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+
+//check the answers
+  userAnswers.forEach((answer, index) => {
+    if (answer === correctAnswers[index]){
+      score += 25;
+    }
+  });
+
+//show the result
+  scrollTo(0, 0);
+  result.classList.remove('d-none');
+
+  let output = 0;
+  const timer = setInterval(() => {
+    result.querySelector('span').textContent = `${output}%`;
+    if(output === score){
+      clearInterval(timer);
+    } else {
+      output++;
+    }
+  }, 10);
+});
+
+
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
+const search = document.querySelector('.search input');
+
+
+const todosToggle = () => {
+    let x = document.querySelector(".container");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  };
+
+  const quizToggle = () => {
+    let y = document.querySelector(".quizContainer");
+    if (y.style.display === "none") {
+      y.style.display = "block";
+    } else {
+      y.style.display = "none";
+    }
+  };
+
+const generateTemplate = todo => {
+
+    const html = ` 
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${todo}</span>
+        <i class="far fa-trash-alt delete"></i>
+      </li>
+    `;
+
+    list.innerHTML += html;
+};
+
+addForm.addEventListener('submit', e => {
+   
+   
+    e.preventDefault();
+    const todo = addForm.add.value.trim();
+
+    if(todo.length){
+        generateTemplate(todo);
+        addForm.reset();
+    }
+});
+
+//Delete todos
+list.addEventListener('click', e => {
+    if(e.target.classList.contains('delete')){
+        e.target.parentElement.remove();
+    }
+});
+
+//Search 
+
+const filterTodos = (term) => {
+
+
+ Array.from(list.children)
+  .filter((todo) => !todo.textContent.toLowerCase().includes(term))
+  .forEach((todo) => todo.classList.add('filtered')) 
+
+  Array.from(list.children)
+  .filter((todo) => todo.textContent.toLowerCase().includes(term))
+  .forEach((todo) => todo.classList.remove('filtered')) 
+};
+
+search.addEventListener('keyup', () => {
+  const term = search.value.trim().toLowerCase();
+  filterTodos(term);
+});
+
